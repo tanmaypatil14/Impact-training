79e60dbfca49e864dfef7c650049725418057c41
tanmaypatil14 <107455709+tanmaypatil14@users.noreply.github.com>
2023-04-14 12:15:49 +0530

    Impact Data Spring Boot
    Impact Data Spring Boot

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,80 +1,21 @@
 package com.example.demo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to SpringBoot using Spingstarter";
+	}
 
-        new SpringApplicationBuilder(DemoApplication.class)
-                .profiles("dev", "prod","qa")
-                .run(args);
-    }
-}
-/*
-@Component
-class MyRunner implements CommandLineRunner {
-
-    @Autowired
-    private Environment environment;
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("Active profiles: " +
-                Arrays.toString(environment.getActiveProfiles()));
-    }
-}*/
-
-@Component
-@Profile(value="dev")
-class MyRunner2 implements CommandLineRunner {
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("In development");
-    }
-}
-
-@Component
-@Profile(value="prod")
-class MyRunner3 implements CommandLineRunner {
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("In production");
-    }
-}
-
-@Component
-@Profile(value="local")
-class MyRunner4 implements CommandLineRunner {
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("In local");
-    }
-}
-
-@Component
-@Profile(value={"dev & local"})
-class MyRunner5 implements CommandLineRunner {
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("In development and local");
-    }
 }
